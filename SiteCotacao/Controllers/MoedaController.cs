bca166238968a21064d54cb05b9ba906d4ebd4fd
Caio64x <caiolimeira10@hotmail.com>
2021-11-04 12:38:36 -0300

    projeto OK

--- SiteCotacao/Controllers/MoedaController.cs
+++ SiteCotacao/Controllers/MoedaController.cs
@@ -27,25 +27,36 @@ namespace SiteCotacao.Controllers
         public ActionResult Converter(FormCollection form)
         {
             //pega o valor do campo da tela
-             decimal valorReal  = Convert.ToDecimal(form["CampoValor"]);
+            decimal valorReal;
+           
+            try
+            {
+                valorReal = Convert.ToDecimal(form["CampoValor"]);
+            }
+            catch 
+            {
+                valorReal = Convert.ToDecimal("0.00");
+            }
+
 
             //pegar o valor da api
             var url = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL";
             var uri = new Uri(url);
             var moeda1 = new USDBRL();
+            Moeda moeda = new Moeda();
 
 
             using(WebClient client = new WebClient())
             {
                 var json = client.DownloadString(uri);
-                moeda1 = JsonConvert.DeserializeObject<USDBRL>(json);
+                moeda = JsonConvert.DeserializeObject<Moeda>(json);
             }
 
 
             ViewBag.TotalReal = valorReal;
-            ViewBag.TotalDolar = valorReal/5;
-            ViewBag.TotalEuro = valorReal/6;
-            ViewBag.TotalBtc = valorReal/3000000;
+            ViewBag.TotalDolar = (valorReal / moeda.USDBRL.ask).ToString("N2");
+            ViewBag.TotalEuro = (valorReal / moeda.EURBRL.ask).ToString("N2");
+            ViewBag.TotalBtc = (valorReal / (moeda.BTCBRL.ask * 1000)).ToString("N4");
 
             return View();
 
--- SiteCotacao/Models/Moeda.cs
+++ SiteCotacao/Models/Moeda.cs
@@ -5,10 +5,58 @@ using System.Web;
 
 namespace SiteCotacao.Models
 {
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
     public class USDBRL
     {
-        public string Code { get; set; }
-        public string Name { get; set; }
-        public string Bid { get; set; }
+        public string code { get; set; }
+        public string codein { get; set; }
+        public string name { get; set; }
+        public string high { get; set; }
+        public string low { get; set; }
+        public string varBid { get; set; }
+        public string pctChange { get; set; }
+        public string bid { get; set; }
+        public decimal ask { get; set; }
+        public string timestamp { get; set; }
+        public string create_date { get; set; }
     }
+
+    public class EURBRL
+    {
+        public string code { get; set; }
+        public string codein { get; set; }
+        public string name { get; set; }
+        public string high { get; set; }
+        public string low { get; set; }
+        public string varBid { get; set; }
+        public string pctChange { get; set; }
+        public string bid { get; set; }
+        public decimal ask { get; set; }
+        public string timestamp { get; set; }
+        public string create_date { get; set; }
+    }
+
+    public class BTCBRL
+    {
+        public string code { get; set; }
+        public string codein { get; set; }
+        public string name { get; set; }
+        public string high { get; set; }
+        public string low { get; set; }
+        public string varBid { get; set; }
+        public string pctChange { get; set; }
+        public string bid { get; set; }
+        public decimal ask { get; set; }
+        public string timestamp { get; set; }
+        public string create_date { get; set; }
+    }
+
+    public class Moeda
+    {
+        public USDBRL USDBRL { get; set; }
+        public EURBRL EURBRL { get; set; }
+        public BTCBRL BTCBRL { get; set; }
+    }
+
+
 }
